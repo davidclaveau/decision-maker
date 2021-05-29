7cbcdf594da62177b2e6b3c3d9fdf353168a3480
davidclaveau <daveclaveau@gmail.com>
2021-05-29 01:23:11 +0000

    Add some more options to our seeds

--- db/seeds/03_options.sql
+++ db/seeds/03_options.sql
@@ -6,3 +6,27 @@ VALUES ('No','Big NONO! I hate ice-creams!', 1);
 
 INSERT INTO options (title, description, poll_id)
 VALUES ('IDC','I DO NOT CARE!', 1);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Vancouver Canucks','Love the hometown heroes!', 5);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Pittsburgh Penguins','Sid the kid!', 5);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Winnipeg Jets','They''re a great team!', 5);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Danny DeVito','What a great actor! I loved them in that one movie they were in!', 6);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Meryl Streep','What a great actor! I loved them in that one movie they were in!', 6);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Brad Pitt','What a great actor! I loved them in that one movie they were in!', 6);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Morgan Freeman','What a great actor! I loved them in that one movie they were in!', 6);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('Glenn Close','What a great actor! I loved them in that one movie they were in!', 6);
