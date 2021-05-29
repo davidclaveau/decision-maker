ef115924725a18e7dee790572ce16c9c4095e504
Zio <zi.ao.tan@outlook.com>
2021-05-29 00:59:56 +0000

    Add email data in seeds files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email) VALUES ('Alice', 'alice.gamil.com');
+INSERT INTO users (name, email) VALUES ('Kira','kira.gamil.com');
