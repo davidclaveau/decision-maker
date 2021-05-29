64eaed0b54ee50893a4b81da8a8b6d6a6a725d14
davidclaveau <daveclaveau@gmail.com>
2021-05-29 00:59:26 +0000

    Add some seeds to our polls table

---
+++ db/seeds/02_polls.sql
@@ -0,0 +1,9 @@
+-- polls table seeds here (Example)
+INSERT INTO polls (name, owner_id) VALUES ('Do you like ice cream?', 1);
+INSERT INTO polls (name, owner_id) VALUES ('What movie should we go see?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('Which restaurant should we go to?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('What''s your favourite number?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('Who is your favourite hockey team?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('Who''s your favourite actor?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('When should we meet up to code?', 2);
+INSERT INTO polls (name, owner_id) VALUES ('What''s the best place on earth?', 2);
