e1f20e63a86966df7ad18d6f5ee63cf439a677de
Zio <zi.ao.tan@outlook.com>
2021-05-29 01:15:29 +0000

    Add fake option rows for poll 1

---
+++ db/seeds/03_options.sql
@@ -0,0 +1,8 @@
+INSERT INTO options (title, description, poll_id)
+VALUES ('Yes','Yeah! I Love ice-creams!', 1);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('No','Big NONO! I hate ice-creams!', 1);
+
+INSERT INTO options (title, description, poll_id)
+VALUES ('IDC','I DO NOT CARE!', 1);
