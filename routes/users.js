fcadbcd1f60c5c422bb8e1f9b1f56ce4c8f23991
davidclaveau <daveclaveau@gmail.com>
2021-05-29 18:39:21 +0000

    Add some routes and queries for our db; work in progress

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,44 @@
 
 const express = require('express');
 const router  = express.Router();
+const pollQueries = require('../lib/poll-queries');
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+// GET /api/polls/
+router.get('/', (req, res) => {
+  pollQueries.getPolls()
+    .then(polls => {
+      res.json(polls);
+    })
+    .catch(err => {
+      console.log("What do you want from me? 1", err)
+    });
+});
+
+// GET /api/polls/:id
+router.get('/:id', (req, res) => {
+  pollQueries.getPollById(req.params.id)
+    .then(poll => {
+      res.json(poll);
+    })
+    .catch(err => {
+      console.log("What do you want from me? 2", err)
+    });
+});
+
+module.exports = router;
+
+// (db) => {
+//   router.get("/", (req, res) => {
+//     db.query(`SELECT * FROM users;`)
+//       .then(data => {
+//         const users = data.rows;
+//         res.json({ users });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
