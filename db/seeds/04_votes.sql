06856c451c74362b269060e901d115243c8a0a0a
davidclaveau <daveclaveau@gmail.com>
2021-05-29 17:01:44 +0000

    Provide a fix to votes seeds

--- db/seeds/04_votes.sql
+++ db/seeds/04_votes.sql
@@ -1,193 +1,155 @@
--- fav num 1
+-- fav num user 1
 INSERT INTO votes (rank, option_id)
 VALUES (1, 12);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 12);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 12);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 12);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 12);
-
--- fav num 2
 INSERT INTO votes (rank, option_id)
 VALUES (2, 13);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 13);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 13);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 13);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 13);
-
--- fav num 3
-INSERT INTO votes (rank, option_id)
-VALUES (3, 14);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 14);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 14);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 14);
-
 INSERT INTO votes (rank, option_id)
 VALUES (5, 14);
-
--- fav num 4
-INSERT INTO votes (rank, option_id)
-VALUES (1, 15);
-
 INSERT INTO votes (rank, option_id)
 VALUES (4, 15);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 15);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 15);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 15);
-
--- fav num 5
-INSERT INTO votes (rank, option_id)
-VALUES (2, 16);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 16);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 16);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 16);
-
 INSERT INTO votes (rank, option_id)
 VALUES (3, 16);
 
--- fav hockey team canucks
+-- fav num user 2
+INSERT INTO votes (rank, option_id)
+VALUES (1, 12);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 13);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 14);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 15);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 16);
+
+-- fav num user 3
+INSERT INTO votes (rank, option_id)
+VALUES (5, 12);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 13);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 14);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 15);
+INSERT INTO votes (rank, option_id)
+VALUES (1, 16);
+
+-- fav num user 4
+INSERT INTO votes (rank, option_id)
+VALUES (2, 12);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 13);
+INSERT INTO votes (rank, option_id)
+VALUES (1, 14);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 15);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 16);
+
+-- fav num user 5
+INSERT INTO votes (rank, option_id)
+VALUES (1, 12);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 13);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 14);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 15);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 16);
+
+-- fav hockey user 1
 INSERT INTO votes (rank, option_id)
 VALUES (3, 17);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 17);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 17);
-
--- fav hockey team pittsburgh
 INSERT INTO votes (rank, option_id)
 VALUES (1, 18);
-
 INSERT INTO votes (rank, option_id)
-VALUES (2, 18);
+VALUES (2, 19);
 
+--- fav hockey user 2
+INSERT INTO votes (rank, option_id)
+VALUES (1, 17);
 INSERT INTO votes (rank, option_id)
 VALUES (3, 18);
-
--- fav hockey team winnipeg
 INSERT INTO votes (rank, option_id)
-VALUES (1, 19);
+VALUES (2, 19);
 
+-- fav hockey user 3
 INSERT INTO votes (rank, option_id)
-VALUES (3, 19);
-
+VALUES (1, 17);
 INSERT INTO votes (rank, option_id)
-VALUES (1, 19);
+VALUES (3, 18);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 19);
 
--- fav actor devito
+-- fav actor user 1
 INSERT INTO votes (rank, option_id)
 VALUES (2, 20);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 20);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 20);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 20);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 20);
-
--- fav actor streep
 INSERT INTO votes (rank, option_id)
 VALUES (1, 21);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 21);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 21);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 21);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 21);
-
--- fav actor pitt
-INSERT INTO votes (rank, option_id)
-VALUES (3, 22);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 22);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 22);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 22);
-
 INSERT INTO votes (rank, option_id)
 VALUES (5, 22);
-
--- fav actor freeman
-INSERT INTO votes (rank, option_id)
-VALUES (1, 23);
-
 INSERT INTO votes (rank, option_id)
 VALUES (4, 23);
-
-INSERT INTO votes (rank, option_id)
-VALUES (3, 23);
-
-INSERT INTO votes (rank, option_id)
-VALUES (2, 23);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 23);
-
--- fav actor close
-INSERT INTO votes (rank, option_id)
-VALUES (2, 24);
-
-INSERT INTO votes (rank, option_id)
-VALUES (1, 24);
-
-INSERT INTO votes (rank, option_id)
-VALUES (4, 24);
-
-INSERT INTO votes (rank, option_id)
-VALUES (5, 24);
-
 INSERT INTO votes (rank, option_id)
 VALUES (3, 24);
 
+-- fav actor user 1
+INSERT INTO votes (rank, option_id)
+VALUES (2, 20);
+INSERT INTO votes (rank, option_id)
+VALUES (1, 21);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 22);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 23);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 24);
 
+-- fav actor user 2
+INSERT INTO votes (rank, option_id)
+VALUES (1, 20);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 21);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 22);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 23);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 24);
 
+-- fav actor user 3
+INSERT INTO votes (rank, option_id)
+VALUES (2, 20);
+INSERT INTO votes (rank, option_id)
+VALUES (1, 21);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 22);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 23);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 24);
 
+-- fav actor user 4
+INSERT INTO votes (rank, option_id)
+VALUES (1, 20);
+INSERT INTO votes (rank, option_id)
+VALUES (5, 21);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 22);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 23);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 24);
+
+-- fav actor user 5
+INSERT INTO votes (rank, option_id)
+VALUES (5, 20);
+INSERT INTO votes (rank, option_id)
+VALUES (3, 21);
+INSERT INTO votes (rank, option_id)
+VALUES (4, 22);
+INSERT INTO votes (rank, option_id)
+VALUES (2, 23);
+INSERT INTO votes (rank, option_id)
+VALUES (1, 24);
