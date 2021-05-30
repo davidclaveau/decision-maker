206eca1539ae18fae2d3ebaf5d07cf12dfd5a5f0
davidclaveau <daveclaveau@gmail.com>
2021-05-30 18:48:09 +0000

    Add a delete button to each inputted option/desc

--- public/scripts/index-add-option.js
+++ public/scripts/index-add-option.js
@@ -1,21 +1,35 @@
 $(document).ready(() => {
 let count = 1
 
-  $("#add-option").click(() => {
+/*
+ * Allow users to input as many options as they like
+ * Create a div with each option and description
+ * Allow users to remove each option
+ */
+
+$("#add-option").click(() => {
     const $optionInput = $("#option-input");
     const $descriptionInput = $("#description-input");
-
+    const $options = $(".options")
     const option = $optionInput.val()
     const description = $descriptionInput.val()
-
-    $(".options").append(`
-      <div>
-        <span>Option ${count}:</span>
+    const el = $(`
+      <div class="option ${count}">
+        <span>Option:</span>
         <input type="text" name="option${count}" value="${option}"/>
         <span>Description:</span>
         <input type="text" name="description${count}" value="${description}"/>
+        <button type="button" class="delete-btn ${count}">Delete</button>
       </div>
-    `);
+    `)
+
+    $options.append(el);
+
+    // Delete each option individually
+    el.on("click", ".delete-btn", () => {
+      el.remove();
+    });
+
     count++;
 
     $optionInput.val("");
