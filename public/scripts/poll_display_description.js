9aa2c7cb4b12f6ddab79d0fb3128d9262787da3d
Zio <zi.ao.tan@outlook.com>
2021-05-31 02:39:00 +0000

    Add toggle desctiption button to see the details of each options

--- public/scripts/poll_display_description.js
+++ public/scripts/poll_display_description.js
@@ -1,11 +1,10 @@
   $(document).ready(() => {
 
-  $('.option_title').click(() => {
+  $('#click').click(() => {
     // alert('click on title')
     // const description=$(this).children('.option_description');
     // $(this).find('p').css
-    $(this).css("background-color","yellow");
-    console.log("Clicked",$(this).data);
+    $('.option_description').slideToggle();
   })
 });
 
