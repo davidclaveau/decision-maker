bffe0527bafe6caf5107a9e394a6bbe09c6019c2
Zio <zi.ao.tan@outlook.com>
2021-05-31 16:16:45 +0000

    Add function: click title to toggle description

--- public/scripts/poll_display_description.js
+++ public/scripts/poll_display_description.js
@@ -1,13 +1,6 @@
   $(document).ready(() => {
-
-  $('.click').click(function() {
-    // alert('click on title')
-    // const description=$(this).children('.option_description');
-    // console.log("clicked", $(this).html())
+  $('.option_title').click(function() {
     $(this).parent().find('.option_description').slideToggle()
-
-    // $('.option_description').css('background-color','red');
-    // console.log($('.option_description').parent().children().slideToggle())
   })
 });
 
