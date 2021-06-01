658e391faacb371843a0d5a7fd5d9c0c72173a0f
Zio <zi.ao.tan@outlook.com>
2021-06-01 03:10:24 +0000

    Create scss for result page and poll page

--- public/scripts/poll_display_description.js
+++ public/scripts/poll_display_description.js
@@ -1,7 +1,7 @@
   $(document).ready(() => {
   $('.option_description').hide()
   $('.option_title').click(function() {
-    $(this).find('.option_description').slideToggle()
+    $(this).parent().find('.option_description').slideToggle()
   })
 });
 
