f6fa4e4e89d2acae37c55f9939d80b343c5e2f84
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-01 05:07:16 +0000

    add cookie test function

--- routes/users.js
+++ routes/users.js
@@ -11,40 +11,40 @@ const userQueries = require('../lib/user-queries');
 
 // GET /users/:id
 router.get('/:id', (req, res) => {
-  // console.log(req.session);
-  console.log("i am in router",req.params.id)
+  //if cookie dosen't match url parameter, send an error menssage, and redirect to login page
+  if (req.params.id !== req.cookies.user_id) {
+    res.status(401).send('You are not allowed');
+    res.redirect('/login');
+  } else {
+    userQueries.getAllPollByUserId(req.params.id)
+      .then(user => {
+        const userId = user[0].user_id;
 
-  res.cookie("user_id", req.params.id);
+        const userName = user[0].user_name;
 
-  userQueries.getAllPollByUserId(req.params.id)
-    .then(user => {
-      const userId = user[0].user_id;
+        //define a pollMap to store one user's all polls and their corresponding options
+        const pollMap = new Map();
+        //define a pollName to store one user's all poll id and poll name
+        const pollName = new Map();
 
-
-      const userName = user[0].user_name;
-
-      //define a pollMap to store one user's all polls and their corresponding options
-      const pollMap = new Map();
-      //define a pollName to store one user's all poll id and poll name
-      const pollName = new Map();
-
-      //put one user's all polls and their options in pollMap
-      for (const obj of user) {
-        if (!pollMap.has(obj.poll_id)) {
-          pollMap.set(obj.poll_id, new Set());
+        //put one user's all polls and their options in pollMap
+        for (const obj of user) {
+          if (!pollMap.has(obj.poll_id)) {
+            pollMap.set(obj.poll_id, new Set());
+          }
+          let eachPoll = pollMap.get(obj.poll_id);
+          eachPoll.add({ optionId: obj.option_id, optionTitle: obj.option_title, rank: obj.sum_rank });
+          //put one user's all poll names in pollName
+          pollName.set(obj.poll_id, obj.poll_name);
         }
-        let eachPoll = pollMap.get(obj.poll_id);
-        eachPoll.add({ optionId: obj.option_id, optionTitle: obj.option_title, rank: obj.sum_rank });
-        //put one user's all poll names in pollName
-        pollName.set(obj.poll_id, obj.poll_name);
-      }
 
-      templateVars = { userId, userName, pollName, pollMap }
-      res.render("users.ejs", templateVars);
-    })
-    .catch(err => {
-      console.log("Error:", err)
-    });
+        templateVars = { userId, userName, pollName, pollMap }
+        res.render("users.ejs", templateVars);
+      })
+      .catch(err => {
+        console.log("Error:", err)
+      });
+  }
 });
 
 
